1cb8fc661ae75b42b1a5d98c26bad74eb6648b90
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-10-16 06:31:01 +0200

    test(dummy): add dummy test to check CI

---
+++ tests/test_dummy.py
@@ -0,0 +1,11 @@
+class TestDummy:
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_dummy(self):
+        print("Hello World !")
