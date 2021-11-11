40c49304d971c52ba5995153c399c384449935de
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-11-11 07:05:55 +0100

    feat(test): add test for pruner conv-linear

--- tests/test_pruner_conv_linear.py
+++ tests/test_pruner_conv_linear.py
@@ -2,21 +2,29 @@ import os
 import random
 
 import numpy as np
+import pytest
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 import torchcompress as tc
 
-# from unittest.mock import Mock
-
-# from torchcompress.node import OPTYPE, Node
-
 
 class TestPruner:
     @classmethod
-    def setup_class(cls):
-        cls._seed_everything(42)
+    def _seed_everything(cls, seed: int):
+        random.seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+
+    @pytest.fixture(autouse=True, scope="function")
+    def setup_and_teardown_at_each_test(self):
+
+        self._seed_everything(42)
 
         class SimpleNet(nn.Module):
             def __init__(self):
@@ -35,53 +43,49 @@ class TestPruner:
                 x = F.relu(x)
                 return x
 
-        # from torchviz import make_dot
+        # Setup
+        self.model = SimpleNet()
+        self.x = torch.randn(1, 2, 4, 4)
+        self.DG = tc.DependencyGraph(self.model)
+        self.DG.build_dependency_graph(self.x)
 
-        cls.model = SimpleNet()
-        x = torch.randn(1, 2, 4, 4)
-        # g = make_dot(cls.model(x))
-        # g.render(filename="graph-full")
+        yield  # Test will be run here
 
-        cls.DG = tc.DependencyGraph(cls.model)
-        cls.DG.build_dependency_graph(x)
-        # print()
+        # Teadown
+        del self.model
+        del self.DG
 
-        # print("===GRAPH===")
-        # for key, value in cls.DG.module_to_node.items():
-        #     print(key)
-        #     print(f"\t{value.outputs}")
-        #     print('----')
+    def test_expand_indices_1(self):
+        pruner = tc.Pruner(DG=self.DG, dummy_input=self.x)
+        node = self.DG.module_to_node[self.model.conv1]
+        nb_filter_before_pruning = self.model.conv1.weight.shape[0]
+        new_indices = pruner._Pruner__expand_indices(
+            input_node=node,
+            indices=[0],
+            nb_filter_before_pruning=nb_filter_before_pruning,
+        )
+        assert np.array_equal(new_indices, [0, 1, 2, 3])
 
-        # print("===DEPENDENCIES===")
-        # for key, value in cls.DG.dependencies.items():
-        #     print(key.module)
-        #     for val in value:
-        #         print(f"\t{val}")
-        #     print("----")
+    def test_expand_indices_2(self):
+        pruner = tc.Pruner(DG=self.DG, dummy_input=self.x)
+        node = self.DG.module_to_node[self.model.conv1]
+        nb_filter_before_pruning = self.model.conv1.weight.shape[0]
+        new_indices = pruner._Pruner__expand_indices(
+            input_node=node,
+            indices=[0, 3],
+            nb_filter_before_pruning=nb_filter_before_pruning,
+        )
+        assert np.array_equal(new_indices, [0, 1, 2, 3, 12, 13, 14, 15])
 
-    @classmethod
-    def teardown_class(cls):
-        del cls.model
-        del cls.DG
-
-    @classmethod
-    def _seed_everything(cls, seed: int):
-        random.seed(seed)
-        os.environ["PYTHONHASHSEED"] = str(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = True
-
-    def test_flatten(self):
-        x = torch.randn(1, 2, 4, 4)
-        pruner = tc.Pruner(DG=self.DG, dummy_input=x)
+    def test_flatten_1(self):
+        pruner = tc.Pruner(DG=self.DG, dummy_input=self.x)
 
         assert self.model.conv1.in_channels == 3
         assert self.model.conv1.out_channels == 4
+        assert self.model.conv1.bias.shape == torch.Size([4])
         assert self.model.linear1.in_features == 16
         assert self.model.linear1.out_features == 8
+        assert self.model.linear1.bias.shape == torch.Size([8])
 
         pruner.run(
             layer=self.model.conv1, criteria=tc.random_criteria, amount_to_prune=0.25
@@ -89,17 +93,32 @@ class TestPruner:
 
         assert self.model.conv1.in_channels == 3
         assert self.model.conv1.out_channels == 3
+        assert self.model.conv1.bias.shape == torch.Size([3])
         assert self.model.linear1.in_features == 12
         assert self.model.linear1.out_features == 8
+        assert self.model.linear1.bias.shape == torch.Size([8])
 
-        assert self.model(x).shape == torch.Size([8])
+        assert self.model(self.x).shape == torch.Size([8])
 
-    def test_expand_indices(self):
-        # [0] -> [0, 1, 2, 3]
-        # [1] -> [4, 5, 6, 7]
-        # [2] -> [8, 9, 10, 11]
-        # [3] -> [12, 13, 14, 15]
+    def test_flatten_2(self):
+        pruner = tc.Pruner(DG=self.DG, dummy_input=self.x)
 
-        # [0, 1] -> [0, 1, 2, 3] + [4, 5, 6, 7] => [0, 1, 2, 3, 4, 5, 6, 7]
-        # [0, 2] -> [0, 1, 2, 3] + [8, 9, 10, 11] => [0, 1, 2, 3, 8, 9, 10, 11]
-        pass
+        assert self.model.conv1.in_channels == 3
+        assert self.model.conv1.out_channels == 4
+        assert self.model.conv1.bias.shape == torch.Size([4])
+        assert self.model.linear1.in_features == 16
+        assert self.model.linear1.out_features == 8
+        assert self.model.linear1.bias.shape == torch.Size([8])
+
+        pruner.run(
+            layer=self.model.conv1, criteria=tc.random_criteria, amount_to_prune=0.75
+        )
+
+        assert self.model.conv1.in_channels == 3
+        assert self.model.conv1.out_channels == 1
+        assert self.model.conv1.bias.shape == torch.Size([1])
+        assert self.model.linear1.in_features == 4
+        assert self.model.linear1.out_features == 8
+        assert self.model.linear1.bias.shape == torch.Size([8])
+
+        assert self.model(self.x).shape == torch.Size([8])
