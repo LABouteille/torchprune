4e9130cefa84c38c95abe4d997eca824251b1dfc
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-11-18 07:01:40 +0100

    chore(test): put pruning test into directory

--- tests/local_tests.sh
+++ tests/local_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-python -m pytest test_dependency_graph.py -s -vv
-python -m pytest test_criteria.py -s -vv
-python -m pytest test_pruner_conv.py -s -vv
-python -m pytest test_pruner_linear.py -s -vv
-python -m pytest test_pruner_conv_linear.py -s -vv
-python -m pytest test_pruner_batchnorm.py -s -vv
-python -m pytest test_pruner.py -s -vv
+python -m pytest pruner/test_dependency_graph.py -s -vv
+python -m pytest pruner/test_criteria.py -s -vv
+python -m pytest pruner/test_pruner_conv.py -s -vv
+python -m pytest pruner/test_pruner_linear.py -s -vv
+python -m pytest pruner/test_pruner_conv_linear.py -s -vv
+python -m pytest pruner/test_pruner_batchnorm.py -s -vv
+python -m pytest pruner/test_pruner.py -s -vv
+#python -m pytest pruner/test_pruner_skip_connection.py -s -vv
